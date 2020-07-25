b81b00fa86715665ea82488b8e9847cdd2beeeaf
Sameer <sameer.libang@gmail.com>
2020-07-25 22:14:25 +1000

    adding morestrings package

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "github.com/alliesground/hello/morestrings"
+)
 
 func main() {
   fmt.Println("Hello, Sam.")
+  fmt.Println(morestrings.ReverseRunes("!oG, olleH"))
 }
